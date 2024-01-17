c8b4d2a8aea2700979a219b0658bc7c7776de45a
Samarth <samarthkulshrestha518@gmail.com>
2024-01-18 01:32:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import React from "react";
+import Home from "./pages/home/HomeScreen";
+// import "./pages/home/styles.css";
 
 function App() {
-  return <div className="App">csi</div>;
+  return <div className="App">
+  <Home />
+  </div>;
 }
 
 export default App;
